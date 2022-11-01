7b8ba9d4c8783d6ea5151b91eafb2071ad86653e
Alexey <clasher@bk.ru>
2022-11-02 00:37:37 +0300

    Добавил тарифы

--- src/angular/app/pages/pages-routing.module.ts
+++ src/angular/app/pages/pages-routing.module.ts
@@ -2,19 +2,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainComponent } from './main/main.component';
+import { TariffComponent } from './tariff/tariff.component';
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
     {
-      path: 'dashboard',
-      component: DashboardComponent,
+      path: 'main',
+      component: MainComponent,
+    },
+    {
+      path: 'tariff',
+      component: TariffComponent,
     },
     {
       path: '',
-      redirectTo: 'dashboard',
+      redirectTo: 'main',
       pathMatch: 'full',
     },
   ],
